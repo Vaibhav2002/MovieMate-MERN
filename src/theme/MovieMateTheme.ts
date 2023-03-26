2a76a038f43971765871a66b9708c44ea2e5d4dc
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-03-26 21:31:54 +0530

    Theming fixed

--- src/theme/MovieMateTheme.ts
+++ src/theme/MovieMateTheme.ts
@@ -1,4 +1,4 @@
-import {createTheme} from "@mui/material/styles";
+import {createTheme} from "@mui/material";
 import colorPalette from "@/theme/ColorPalette";
 import typography from "@/theme/Typography";
 
