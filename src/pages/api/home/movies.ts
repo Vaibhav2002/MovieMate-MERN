4e1053a495d115681c088f6549b20506026e94a8
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-03-26 21:31:40 +0530

    home endpoint structure changed

--- src/pages/api/home/movies.ts
+++ src/pages/api/home/movies.ts
@@ -1,8 +1,10 @@
 import * as datasource from "@/data/datasource/MoviesDataSource";
-import {HomeData, MovieSectionList} from "@/data/models/local/HomeData";
+import {MovieSectionList} from "@/data/models/local/HomeData";
 import {NextApiRequest, NextApiResponse} from "next";
 import {isFulfilled} from "@/data/utils/PromiseFulfilled";
-import MovieSection, {movieSections} from "@/data/models/local/MovieSection";
+import MovieSection from "@/data/models/local/MovieSection";
+import {addUrlToMovie} from "@/data/utils/ImageUrlHelper";
+
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const trendingMovies = datasource.fetchTrendingMovies()
@@ -12,38 +14,30 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const topRatedMovies = datasource.fetchTopRatedMovies()
     const popularMovies = datasource.fetchPopularMovies()
 
-    const names = movieSections
+
+    const homeSections = [
+        MovieSection.Trending,
+        MovieSection.NowPlaying,
+        MovieSection.Upcoming,
+        MovieSection.Latest,
+        MovieSection.TopRated,
+        MovieSection.Popular
+    ]
 
     const responses = await Promise.allSettled(
         [trendingMovies, nowPlayingMovies, upcomingMovies, latestMovies, topRatedMovies, popularMovies]
     )
 
-    const imageBaseUrl = "https://image.tmdb.org/t/p"
-    const posterSize = "w500"
-    const backdropSize = "w780"
-
-    const getPosterUrl = (file: string) => `${imageBaseUrl}/${posterSize}${file}`
-    const getBackdropUrl = (file: string) => `${imageBaseUrl}/${backdropSize}${file}`
-
     const movieList = responses.filter(isFulfilled)
         .map(response => response.value)
         .map((result, index): MovieSectionList => ({
-            header: names[index],
-            movies: result.results ? result.results.map(movie => {
-                return {
-                    ...movie,
-                    poster_path: getPosterUrl(movie.poster_path),
-                    backdrop_path: getBackdropUrl(movie.backdrop_path)
-                }
-            }) : []
+            header: homeSections[index],
+            movies: result.results ? result.results.map(addUrlToMovie) : []
         }))
         .map(list => (list.header === MovieSection.Trending)
             ? {...list, movies: list.movies.slice(0, 8)}
-            : list
-        )
+            : list)
         .filter(data => data.movies.length > 0)
 
-    const homeData: HomeData = {data: movieList}
-
-    return res.status(200).json(homeData)
+    return res.status(200).json(movieList)
 }
