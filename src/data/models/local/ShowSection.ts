791a66b4f7060ecaed66281c4632ffb917876f55
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-03-26 21:30:12 +0530

    Added Show Model and Sections

---
+++ src/data/models/local/ShowSection.ts
@@ -0,0 +1,9 @@
+enum ShowSection {
+    AiringToday = "Airing Today",
+    OnTheAir = "On The Air",
+    Popular = "Popular",
+    TopRated = "Top Rated",
+    Latest = "Latest",
+}
+
+export default ShowSection
