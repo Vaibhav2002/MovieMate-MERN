245d73eda22eb8e64255fc807b3fb01e8342078f
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-03-26 21:35:12 +0530

    added shows to homeData

--- src/data/models/local/HomeData.ts
+++ src/data/models/local/HomeData.ts
@@ -1,11 +1,14 @@
 import Movie from "@/data/models/dto/movie/Movie";
 import MovieSection from "@/data/models/local/MovieSection";
-
-export interface HomeData {
-    data: MovieSectionList[]
-}
+import ShowSection from "@/data/models/local/ShowSection";
+import {Show} from "@/data/models/dto/movie/Show";
 
 export interface MovieSectionList {
     header: MovieSection
     movies: Movie[]
+}
+
+export interface ShowSectionList {
+    header: ShowSection
+    shows: Show[]
 }
