3843a050fc23a20e0a3713f5d853656842d258e9
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-03-26 17:45:06 +0530

    refactoring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import {ThemeProvider} from "@mui/system";
+import type {AppProps} from 'next/app'
+import {ThemeProvider} from "@mui/material";
 import movieMateTheme from "@/theme/MovieMateTheme";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-      <ThemeProvider theme={movieMateTheme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-  )
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <ThemeProvider theme={movieMateTheme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
 }
