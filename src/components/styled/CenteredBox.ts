6c0ed62db2bb0de08eb356df2d747ec8e09041d6
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-03-28 21:23:01 +0530

    util component

---
+++ src/components/styled/CenteredBox.ts
@@ -0,0 +1,9 @@
+import {Box, styled} from "@mui/material";
+
+const CenteredBox = styled(Box)(() => ({
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+}))
+
+export default CenteredBox
