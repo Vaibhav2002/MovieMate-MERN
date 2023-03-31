f1f00b546df3c2cb145df5984265bf461b157d1e
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-03-31 18:03:33 +0530

    ProgressBar added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
 import {ThemeProvider} from "@mui/material";
 import movieMateTheme from "@/theme/MovieMateTheme";
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({Component, pageProps}: AppProps) {
     return (
         <ThemeProvider theme={movieMateTheme}>
+            <NextNProgress/>
             <Component {...pageProps} />
         </ThemeProvider>
     )
