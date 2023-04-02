88b87618d78bbb256bad8590d455ee2063c0c3fd
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-04-02 16:56:00 +0530

    DiscoverGenre screen added

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,5 +1,6 @@
 import Section from "@/data/models/local/Section";
 import {getSlugFromSection} from "@/data/utils/SectionUtils";
+import Genre from "@/data/models/dto/Genre";
 
 enum Routes {
     HOME = "/home",
@@ -7,13 +8,12 @@ enum Routes {
     MOVIE = "/movie",
 
     DISCOVER = "/discover",
-    GENRE = "/genre",
 }
 
-export const getSectionRoute = (section: Section, page:number = 1) => `${Routes.DISCOVER}/${getSlugFromSection(section)}?page=${page}`
+export const getSectionRoute = (section: Section, page: number = 1) => `${Routes.DISCOVER}/${getSlugFromSection(section)}?page=${page}`
 
 export const getMovieRoute = (id: number) => `${Routes.MOVIE}/${id}`
 
-export const getGenreRoute = (genre: number) => `${Routes.GENRE}/${genre}`
+export const getGenreRoute = (genre: Genre, page: number = 1) => `${Routes.DISCOVER}/genre/${genre.id}?genre=${genre.name}&&page=${page}`
 
 export default Routes
---
+++ src/pages/discover/genre/[id].tsx
@@ -0,0 +1,61 @@
+import Movie from "@/data/models/dto/Movie";
+import Genre from "@/data/models/dto/Genre";
+import {GetServerSideProps} from "next";
+import {fetchMoviesByGenre} from "@/data/datasource/MoviesDataSource";
+import {Box, Typography} from "@mui/material";
+import MoviesGrid from "@/components/screens/discoverSection/MoviesGrid";
+import PageNavigationItem from "@/components/PageNavigationItem";
+import React from "react";
+import {useRouter} from "next/router";
+import {getGenreRoute} from "@/Routes";
+
+
+export const getServerSideProps: GetServerSideProps<DiscoverGenreProps> = async (context) => {
+    const id = parseInt(context.params!.id as string)
+    const {page, genre} = context.query
+    const response = await fetchMoviesByGenre(id, parseInt(page as string))
+    return {
+        props: {
+            genre: {id: id, name: genre} as Genre,
+            movies: response.results,
+            page: response.page,
+            isLastPage: response.page === response.total_pages
+        }
+    }
+}
+
+interface DiscoverGenreProps {
+    genre: Genre
+    movies: Movie[]
+    page: number
+    isLastPage: boolean
+}
+
+
+const DiscoverGenreScreen = ({genre, movies, page, isLastPage}: DiscoverGenreProps) => {
+    const router = useRouter()
+    const isFirstPage = page === 1
+    const nextPage = () => router.push(getGenreRoute(genre, page + 1))
+    const prevPage = () => router.back()
+
+
+    return (
+        <Box sx={{overflowX: "hidden"}} padding={4}>
+            <Typography variant="h4" marginBottom={3}>{genre.name}</Typography>
+
+            <MoviesGrid movies={movies}/>
+
+            <Box marginY={4}>
+                <PageNavigationItem
+                    page={page}
+                    isLastPage={isLastPage}
+                    isFirstPage={isFirstPage}
+                    onNextPage={nextPage}
+                    onPrevPage={prevPage}/>
+            </Box>
+
+        </Box>
+    )
+}
+
+export default DiscoverGenreScreen
