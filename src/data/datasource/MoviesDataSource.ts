27e8e0e91a402082931eea33becf1b154caa5daf
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-04-02 15:22:20 +0530

    added pagination support to MoviesDataSource.ts

--- src/data/datasource/MoviesDataSource.ts
+++ src/data/datasource/MoviesDataSource.ts
@@ -1,19 +1,35 @@
 import {MoviesResponse} from "@/data/models/dto/Movie";
 import api from "@/data/datasource/TMDBApiClient";
+import {addUrlToMovie} from "@/data/utils/ImageUrlHelper";
 
-const fetchMovies = async (url:string):Promise<MoviesResponse> => {
-    const response = await api.get<MoviesResponse>(url)
+const fetchMovies = async (url:string, pageNo:string):Promise<MoviesResponse> => {
+    const response = await api.get<MoviesResponse>(url, {
+        params: {
+            page: pageNo
+        }
+    })
     return response.data
 }
 
-export const fetchTrendingMovies = () => fetchMovies('/trending/movie/week')
+const addUrlToMovies = (response:MoviesResponse) => ({
+    ...response,
+    results: response.results.map(addUrlToMovie)
+})
 
-export const fetchPopularMovies = () => fetchMovies('/movie/popular')
+export const fetchTrendingMovies = (pageNo:string = "1") => fetchMovies('/trending/movie/week', pageNo)
+    .then(addUrlToMovies)
 
-export const fetchLatestMovies = () => fetchMovies("/movie/latest")
+export const fetchPopularMovies = (pageNo:string = "1") => fetchMovies('/movie/popular', pageNo)
+    .then(addUrlToMovies)
 
-export const fetchTopRatedMovies = () => fetchMovies("movie/top_rated")
+export const fetchLatestMovies = (pageNo:string = "1") => fetchMovies("/movie/latest", pageNo)
+    .then(addUrlToMovies)
 
-export const fetchNowPlayingMovies = () => fetchMovies("movie/now_playing")
+export const fetchTopRatedMovies = (pageNo:string = "1") => fetchMovies("movie/top_rated", pageNo)
+    .then(addUrlToMovies)
 
-export const fetchUpcomingMovies = () => fetchMovies("movie/upcoming")
+export const fetchNowPlayingMovies = (pageNo:string = "1") => fetchMovies("movie/now_playing", pageNo)
+    .then(addUrlToMovies)
+
+export const fetchUpcomingMovies = (pageNo:string = "1") => fetchMovies("movie/upcoming", pageNo)
+    .then(addUrlToMovies)
