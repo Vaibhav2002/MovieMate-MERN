594e256aa693d58f56827855bc6f3983a4a2a32d
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-04-03 13:42:27 +0530

    bug fixing

--- src/data/utils/Helpers.ts
+++ src/data/utils/Helpers.ts
@@ -4,8 +4,8 @@ export function notNullOPrEmpty<T>(value: T[]): asserts value is NonNullable<Arr
     }
 }
 
-export function assertIsDefined<T>(value: T): asserts value is NonNullable<T> {
-    if (!value) throw Error("Expected 'val' to be defined but received " + value)
+export function assertIsDefined<T>(value: T, message?:string): asserts value is NonNullable<T> {
+    if (!value) throw Error(`Expected ${message ?? `val`} to be defined but received ${value}`)
 }
 
 export const roundTo2Decimals = (value: number) => Math.round(value * 100) / 100
