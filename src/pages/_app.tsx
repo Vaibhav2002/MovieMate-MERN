647907c593a6c6bcfadf47644bd1b588f5c4ddd9
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-04-06 20:28:43 +0530

    Analytics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import type {AppProps} from 'next/app'
 import {ThemeProvider} from "@mui/material";
 import movieMateTheme from "@/theme/MovieMateTheme";
 import NextNProgress from 'nextjs-progressbar';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({Component, pageProps}: AppProps) {
     return (
         <ThemeProvider theme={movieMateTheme}>
             <NextNProgress/>
             <Component {...pageProps} />
+            <Analytics/>
         </ThemeProvider>
     )
 }
